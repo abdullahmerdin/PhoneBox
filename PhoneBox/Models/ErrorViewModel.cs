2bb6cb7165bb49179b1b5c77878aedd36e963336
Abdullah Merdin <abdullahmerdin@gmail.com>
2022-07-21 00:06:41 +0300

    Add project files.

---
+++ PhoneBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhoneBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
